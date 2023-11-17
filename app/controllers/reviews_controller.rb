1450df8235b1e67ba490483bc9f44494420abb84
Maxime Arion <maxime.arion.pro@gmail.com>
2023-11-17 16:51:15 +0100

    rails-yelp-mvp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,32 @@
+class ReviewsController < ApplicationController
+  def index
+    @review = Review.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "restaurants/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
